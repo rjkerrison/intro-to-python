835bfee1bf67d95ba5b4ace9e11ac32ae050953d
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-19 20:14:19 +0200

    or and and

--- booleans.py
+++ booleans.py
@@ -1,3 +1,9 @@
 # Capitalise the first letter
 is_this_python = True
 is_this_javascript = False
+
+if is_this_python or is_this_javascript:
+    print("It is a programming language")
+
+if is_this_python and is_this_javascript:
+    print("It is a weird hybrid language, BEWARE")
