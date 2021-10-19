ce5fc27ff29c98cdbfcec473814d720292bbc082
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-19 21:31:07 +0200

    dicts

---
+++ dicts.py
@@ -0,0 +1,12 @@
+person = {
+    "name": "Robin",
+    "github_username": "rjkerrison",
+    "is_human": True,
+}
+
+print(person)
+print(person["name"])
+print(person["is_human"])
+
+person["surname"] = "Kerrison"
+print(person)
