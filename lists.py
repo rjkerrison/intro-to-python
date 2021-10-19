7940da848cf97075fc954197edb0a33c36414b46
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-19 21:52:44 +0200

    lists

--- lists.py
+++ lists.py
@@ -4,6 +4,9 @@
 numbers = range(10)
 print(numbers)
 
+for i in numbers:
+    print(f"i is {i}")
+
 # What the hell is this thing?
 # It's called a list comprehension!
 # This builds an array from the range.
@@ -16,5 +19,18 @@ numbers_squared = [x ** 2 for x in numbers]
 print(numbers_squared)
 
 # We can even filter with them
+# in pseudocode terms, this list is "x squared for x in numbers if x is even"
 even_squares = [x ** 2 for x in numbers if x % 2 == 0]
 print(even_squares)
+
+
+basic_list = [1, 2, 3]
+# for adding an extra item
+basic_list.append(5)
+# for adding an extra array
+basic_list.extend([2, 3, 4])
+print(basic_list)
+basic_list.remove(5)
+print(basic_list)
+basic_list.insert(4, 178)
+print(basic_list)
