8e36ae309a99671c56a203326b649757e1fc8f52
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-19 20:02:00 +0200

    indentation

---
+++ indentation.py
@@ -0,0 +1,11 @@
+a = 10
+
+# Indentation indicates the if-block
+if a < 5:
+    print("I will not be printed")
+    print("me neither")
+
+# When the indentation stops, the if block is over
+if a < 8:
+    print("I will not be printed")
+print("but I will!")
