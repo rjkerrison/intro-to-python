aecb78a9b3ce162aafa5c0b3079dcbdefc639da0
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-19 21:29:41 +0200

    ternary

--- booleans.py
+++ booleans.py
@@ -7,3 +7,9 @@ if is_this_python or is_this_javascript:
 
 if is_this_python and is_this_javascript:
     print("It is a weird hybrid language, BEWARE")
+
+a = 10
+# ternary operators are a bit odd
+# "b is 20 if a is less than 10 otherwise it's 40"
+b = 20 if a < 10 else 40
+print(b)
